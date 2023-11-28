d25398bc130d97812525fdc39cec2be9791af0c3
TonyGrif <TonyGriffin2000@gmail.com>
2023-11-28 13:49:20 -0500

    create producer threads

--- app/src/test/java/producerconsumer/AppTest.java
+++ app/src/test/java/producerconsumer/AppTest.java
@@ -5,6 +5,7 @@ package producerconsumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,16 @@ class AppTest {
     Assertions.assertThrows(
         IllegalArgumentException.class,
         () -> App.validateArgs(new String[] {"4", "4", "4", "4"}, 3));
+  }
+
+  @Test
+  void testCreateProducers() {
+    ArrayList<Thread> producers;
+
+    producers = App.createProducers(4);
+    assertEquals(4, producers.size());
+
+    producers = App.createProducers(42);
+    assertEquals(42, producers.size());
   }
 }
