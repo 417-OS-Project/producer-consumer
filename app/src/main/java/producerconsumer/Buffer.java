838dfd17c97f702b0b00a147be14e447569ca412
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-02 00:06:51 -0500

    move to array list vs []

--- app/src/main/java/producerconsumer/Buffer.java
+++ app/src/main/java/producerconsumer/Buffer.java
@@ -1,16 +1,25 @@
 package producerconsumer;
 
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
 /** Stores the needed information for a bounded buffer. */
 public class Buffer {
   /** The maximum size of this buffer. */
   private static final int BUFFER_SIZE = 5;
 
+  /**
+   * The mutex lock for this buffer.
+   */
+  private Semaphore mutex;
+
   /** Array of integer values. */
-  private int[] contents;
+  private ArrayList<Integer> contents;
 
   /** Constructor for this buffer. */
   public Buffer() {
-    this.contents = new int[BUFFER_SIZE];
+    mutex = new Semaphore(1);
+    this.contents = new ArrayList<Integer>(BUFFER_SIZE);
   }
 
   /**
@@ -20,5 +29,29 @@ public class Buffer {
    */
   public int getMaximumQuantity() {
     return BUFFER_SIZE;
+  }
+
+  /**
+   * Return the number of elements currently stored in this buffer.
+   *
+   * @return number of elements in content.
+   */
+  public int getNumOfContent() {
+    return this.contents.size();
+  }
+
+  /**
+   * Insert an item into the buffer.
+   *
+   * @return 0 if successful, -1 otherwise.
+   */
+  public int insertItem() throws InterruptedException {
+    System.out.printf("Thread %d seeking entry%n", Thread.currentThread().getId());
+    this.mutex.acquire();
+    System.out.printf("Thread %d entering%n", Thread.currentThread().getId());
+
+    System.out.printf("Thread %d leaving%n", Thread.currentThread().getId());
+    this.mutex.release();
+    return -1;
   }
 }
